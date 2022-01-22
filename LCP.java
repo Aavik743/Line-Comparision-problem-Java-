e9f6acc6f10b96a2e76025703967a9614b7ffa82
unknown <aavik743@gmail.com>
2022-01-22 22:13:25 +0530

    added: compareTo method

--- LCP.java
+++ LCP.java
@@ -8,13 +8,21 @@ public class LCP
     {
         System.out.println ("Welcome to Line Comparison Computation Program");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the co-ordinate x1");
         int x1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y1");
         int y1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x2");
         int x2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y2");
         int y2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x3");
         int x3 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y3");
         int y3 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x4");
         int x4 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y4");
         int y4 = sc.nextInt();
         int l1 = (int) Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
         int l2 = (int) Math.sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));
@@ -22,6 +30,6 @@ public class LCP
         String s2=String.valueOf(l2);
         System.out.println(s1);
         System.out.println(s2);
-        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
     }
 }
